818d0ee8eb056a8cbaeb72e401281c57e4acb9f5
Maxim Kolchin <kolchinmax@gmail.com>
2019-01-21 23:22:43 +0300

    Add equals and hashCode to SPOTriple

--- src/main/kotlin/cc/datafabric/scylladb/pipelines/bulkload/SPOTriple.kt
+++ src/main/kotlin/cc/datafabric/scylladb/pipelines/bulkload/SPOTriple.kt
@@ -6,6 +6,7 @@ import com.datastax.driver.mapping.annotations.PartitionKey
 import com.datastax.driver.mapping.annotations.Table
 import org.eclipse.rdf4j.model.Statement
 import java.io.Serializable
+import java.util.Objects
 
 @Table(name = "spo", keyspace = "triplestore")
 class SPOTriple() : Serializable {
@@ -26,5 +27,22 @@ class SPOTriple() : Serializable {
         this.subject = stmt.subject.stringValue()
         this.predicate = stmt.predicate.stringValue()
         this.`object` = stmt.`object`.stringValue()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if(other == null || !(other is SPOTriple)) {
+            return false
+        }
+
+        return Objects.equals(this.subject, other.subject) &&
+            Objects.equals(this.predicate, other.predicate) &&
+            Objects.equals(this.`object`, other.`object`)
+    }
+
+    override fun hashCode(): Int {
+        var result = subject.hashCode()
+        result = 31 * result + predicate.hashCode()
+        result = 31 * result + `object`.hashCode()
+        return result
     }
 }
